cc77a1766230da5f7bb60571d3415e5ec07ff086
Joao Inez <joaoinez.info@gmail.com>
2024-03-21 05:18:34 +0100

    WIP - Add more configs

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # dotfiles
 
+Install paru:
+
+```sh
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+```
+
 How to run:
+
 ```sh
 chezmoi init --apply $GITHUB_USERNAME
 ```
+
+TODO:
+systemd services: sddm and docker.socket
+change shell from bash to zsh
+grub config
+sddm config and theming
+gtk and qt5 theming
+change docker image location
---
+++ run_once_step-1_nvidia-conf.sh
@@ -0,0 +1,14 @@
+#!/user/bin/env bash
+set -euo pipefail
+
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia_drm.modeset=1"' /etc/default/grub
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)' /etc/mkinitcpio.conf
+
+sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+
+sudo touch /etc/modprobe.d/nvidia.conf
+
+echo 'options nvidia-drm modeset=1' >> /etc/modprobe.d/nvidia.conf
