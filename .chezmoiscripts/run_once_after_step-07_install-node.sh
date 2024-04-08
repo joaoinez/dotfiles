542663db5dbc8a987b4a84b14c58c01b7224cea1
João Inez <joaoinez.info@gmail.com>
2024-04-08 13:38:29 +0200

    Fix nvm script

--- .chezmoiscripts/run_once_after_step-07_install-node.sh
+++ .chezmoiscripts/run_once_after_step-07_install-node.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-exec /usr/bin/zsh
-source ~/.zshrc
-
-echo $(nvm current)
-
-if [[ $(nvm current) == "none" ]]; then
+if [[ $(zsh -c 'source ~/.zshrc && nvm current') == "none" ]]; then
   echo "Installing node"
 
-  nvm install node
+  zsh -c 'source ~/.zshrc && nvm install node'
 fi
