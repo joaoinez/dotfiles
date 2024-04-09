0d932cfb12b28e2420c20e32b66d13a03deded1c
João Inez <joaoinez.info@gmail.com>
2024-04-09 22:52:57 +0200

    Remove Wallpapers/converted/car.jpg Remove Wallpapers/converted/dim-river.jpg Remove Wallpapers/converted/factory-smoke.png Remove Wallpapers/converted/horse.jpg Remove Wallpapers/converted/japan-train.jpg Remove Wallpapers/converted/moon-dune.png Remove Wallpapers/converted/painted-sunset.jpg Remove Wallpapers/converted/pink-wires.jpg Remove Wallpapers/converted/red-dot.jpg Remove Wallpapers/converted/red-mountain.jpg Remove Wallpapers/converted/red-nebula.png Remove Wallpapers/converted/red-skyline.jpg Remove Wallpapers/converted/retro.jpg Remove Wallpapers/converted/small-sunset.jpg Remove Wallpapers/converted/sunset.jpg Remove Wallpapers/converted/tori-gate.jpg Remove Wallpapers/converted/watercolor-clouds.jpg Update .local/bin/resize_images_in_dir_to_wallpaper.sh

--- private_dot_local/bin/resize_images_in_dir_to_wallpaper.sh
+++ private_dot_local/bin/resize_images_in_dir_to_wallpaper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+mkdir wallpapers
+
 for file in *
 do
   if [ -f "$file" ]; then
-      convert $file -resize 2560x1440 "converted/$file"
+      convert $file -resize 2560x1440 "wallpapers/$file"
   fi
 done
