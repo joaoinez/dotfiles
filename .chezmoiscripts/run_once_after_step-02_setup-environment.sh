c82738a5537ce54d16165b29c32e1ed438f258a7
João Inez <joaoinez.info@gmail.com>
2024-11-21 21:14:49 +0100

    Update dotfiles

--- .chezmoiscripts/run_once_after_step-02_setup-environment.sh
+++ .chezmoiscripts/run_once_after_step-02_setup-environment.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 
 echo "Applying sddm theme"
 
-sudo cp -r ~/.local/share/chezmoi/root/etc/sddm.conf.d/theme.conf /etc/sddm.conf.d/theme.conf
+sudo touch /etc/sddm.conf.d/theme.conf
+
+sudo cp ~/.local/share/chezmoi/root/etc/sddm.conf.d/theme.conf /etc/sddm.conf.d/theme.conf
+
+sudo touch /usr/share/sddm/themes/tokyo-night-sddm/theme.conf.user
 
 sudo cp ~/.local/share/chezmoi/root/usr/share/sddm/themes/tokyo-night-sddm/theme.conf.user /usr/share/sddm/themes/tokyo-night-sddm/theme.conf.user
 
