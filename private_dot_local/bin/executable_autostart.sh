6d5df1b2e31c2c5427d9a2089226fa48fe426c48
João Inez <joaoinez.info@gmail.com>
2025-07-30 20:46:09 +0000

    feat: setup autostart script and service

---
+++ private_dot_local/bin/executable_autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# wl-paste --type text --watch cliphist store &
+# wl-paste --type image --watch cliphist store &
+# syshud -p 'bottom-center'-i 24 &
+# sleep 1 && ~/.local/bin/change_wallpaper.sh "$HOME/Pictures/Wallpapers" &
+# gsettings set org.gnome.desktop.interface gtk-theme 'rose-pine-gtk' &
+# gsettings set org.gnome.desktop.interface icon-theme 'rose-pine-icons' &
+# gsettings set org.gnome.desktop.interface cursor-theme 'BreezeX-RosePineDawn-Linux' &
+# gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11' &
+# gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' &
--- private_dot_local/bin/executable_change_wallpaper.sh
+++ private_dot_local/bin/executable_change_wallpaper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 # This script will randomly go through the files of a directory, setting it
 # up as the wallpaper at regular intervals
@@ -8,23 +9,23 @@
 #
 # https://github.com/LGFae/swww/blob/main/example_scripts/swww_randomize.sh
 
-if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
-	echo "Usage:
+if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
+  echo "Usage:
 	$0 <dir containing images>"
-	exit 1
+  exit 1
 fi
 
 # This controls (in seconds) when to switch to the next image
 INTERVAL=900
 
 while true; do
-	find "$1" -type f \
-		| while read -r img; do
-			echo "$((RANDOM % 1000)):$img"
-		done \
-		| sort -n | cut -d':' -f2- \
-		| while read -r img; do
-			swww img --transition-type grow --transition-pos top-right --transition-fps 120 "$img"
-			sleep $INTERVAL
-		done
+  find "$1" -type f |
+    while read -r img; do
+      echo "$((RANDOM % 1000)):$img"
+    done |
+    sort -n | cut -d':' -f2- |
+    while read -r img; do
+      swww img --transition-type grow --transition-pos top-right --transition-fps 120 "$img"
+      sleep $INTERVAL
+    done
 done
