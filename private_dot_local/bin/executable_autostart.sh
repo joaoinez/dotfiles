e996ca1899b3c8bf2d7d386bc0063058c75c7ee6
João Inez <joaoinez.info@gmail.com>
2025-07-31 03:44:27 +0200

    fix: uncomment autostart script

--- private_dot_local/bin/executable_autostart.sh
+++ private_dot_local/bin/executable_autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-# syshud -p 'bottom-center'-i 24 &
-#
-# sleep 1 && /home/astherae/.local/bin/change_wallpaper.sh "/home/astherae/Pictures/Wallpapers" &
-#
-# wl-paste --type text --watch cliphist store &
-# wl-paste --type image --watch cliphist store &
+syshud -p 'bottom-center'-i 24 &
+
+sleep 1 && /home/astherae/.local/bin/change_wallpaper.sh "/home/astherae/Pictures/Wallpapers" &
+
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
 
 exec wlsunset -l 51.9 -L 4.5
